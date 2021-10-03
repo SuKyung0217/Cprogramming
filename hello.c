ebe9c5d8cd6a2e768212a640ee4988ff3816a306
SuKyung0217 <91124029+SuKyung0217@users.noreply.github.com>
2021-10-03 23:52:22 +0900

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void) 
+{
+	printf("+============+\n");
+	printf("|컴퓨터정보과|\n");
+	printf("+------------+\n");
+	printf("|   김수경   |\n");
+	printf("+============+\n");
+	return 0;
+}
